f6f14f86dcbb25ff013a5f14b707e300c09c1987
petumazo <davidalvarezmazzo@gmail.com>
2021-05-21 17:44:22 -0500

    Add file with matching words on tweets

--- lib/text_matching.rb
+++ lib/text_matching.rb
@@ -1,1 +1,10 @@
-@inspire_me = ['insipire']
+# frozen_string_literal: true
+
+@open_source_matching = ['open source', 'opensource', 'free', 'Richard Stallman', 'RMS', 'non-proprietary', 'software',
+                         'FOSS']
+
+@motivate_source_matching = %w[motivate sad encourage vitalize cheer positive negative actitud
+                               pressure unmotivated happy unfortunate lifeless]
+
+@inspire_source_matching = ['inspire', 'stimulate', 'persuade', 'energize', 'work', 'programming', 'hard work', 'lazy',
+                            'work-shy', 'inactive', 'active', 'shiftless', 'indolent', 'spiritless', 'passive']
