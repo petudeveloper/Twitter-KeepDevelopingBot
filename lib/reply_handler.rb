fd9f026e295e57afd3cb18f143aff901823b4fe7
petumazo <davidalvarezmazzo@gmail.com>
2021-05-21 17:41:28 -0500

    Add logic to reply handler

---
+++ lib/reply_handler.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require_relative 'text_matching'
+
+module ReplyHandler
+  def empty_help?(tweet)
+    raise ScriptError.new, 'Only Tweet-Object in #empty_help' if tweet.is_a?(Array)
+
+    ['help', 'what can you do', 'assistance', 'aid', 'support'].each do |i|
+      return true if /\b#{i}\b/i.match?(tweet.text) || tweet.text == '@petudeveloper'
+    end
+    false
+  end
+
+  def opensource?(tweet)
+    raise ScriptError.new, 'Only Tweet-Object in #opensource' if tweet.is_a?(Array)
+
+    @open_source_matching.each do |i|
+      return true if /\b#{i}\b/i.match?(tweet.text)
+    end
+    false
+  end
+
+  def motivate?(tweet)
+    raise ScriptError.new, 'Only Tweet-Object in #motivate' if tweet.is_a?(Array)
+
+    @motivate_source_matching.each do |i|
+      return true if /\b#{i}\b/i.match?(tweet.text)
+    end
+    false
+  end
+
+  def inspire?(tweet)
+    raise ScriptError.new, 'Only Tweet-Object in #inspire' if tweet.is_a?(Array)
+
+    @inspire_source_matching.each do |i|
+      return true if /\b#{i}\b/i.match?(tweet.text)
+    end
+    false
+  end
+end
