50e058d645674b0339fdd379fb59c4ac6e594789
petumazo <davidalvarezmazzo@gmail.com>
2021-05-25 19:36:43 -0500

    Remove unnecessary returns

--- lib/reply_handler.rb
+++ lib/reply_handler.rb
@@ -1,39 +1,34 @@
 require_relative 'text_matching'
 
 module ReplyHandler
+  def word_matcher(arr, tweet)
+    arr.each do |i|
+      return true if /\b#{i}\b/i.match?(tweet.text)
+    end
+    false
+  end
+
   def empty_help?(tweet)
     raise ScriptError.new, 'Only Tweet-Object in #empty_help' if tweet.is_a?(Array)
 
-    @help_source_matching.each do |i|
-      return true if /\b#{i}\b/i.match?(tweet.text) || tweet.text == '@petudeveloper'
-    end
-    false
+    word_matcher(@help_source_matching, tweet)
   end
 
   def opensource?(tweet)
     raise ScriptError.new, 'Only Tweet-Object in #opensource' if tweet.is_a?(Array)
 
-    @open_source_matching.each do |i|
-      return true if /\b#{i}\b/i.match?(tweet.text)
-    end
-    false
+    word_matcher(@open_source_matching, tweet)
   end
 
   def motivate?(tweet)
     raise ScriptError.new, 'Only Tweet-Object in #motivate' if tweet.is_a?(Array)
 
-    @motivate_source_matching.each do |i|
-      return true if /\b#{i}\b/i.match?(tweet.text)
-    end
-    false
+    word_matcher(@motivate_source_matching, tweet)
   end
 
   def inspire?(tweet)
     raise ScriptError.new, 'Only Tweet-Object in #inspire' if tweet.is_a?(Array)
 
-    @inspire_source_matching.each do |i|
-      return true if /\b#{i}\b/i.match?(tweet.text)
-    end
-    false
+    word_matcher(@inspire_source_matching, tweet)
   end
 end
